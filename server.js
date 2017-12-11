1459c409fac4201c9871b1ed387861b0159084c0
tyras <tirasmuturi@gmail.com>
2017-12-11 16:37:32 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var port = 80;
+
+http.createServer(function(req, res){
+  var hostname = process.env.HOSTNAME;
+  res.writeHead(200, { 'Content-Type': 'text/HTML' });
+  var name = process.env.NAME || 'World';
+  var body = 
+    '<h1>Hello '+name+'</h1> \
+     <p>This is a demo for <em>blue-green deployment</em></p> \
+     <p>from server: '+hostname+'</p> \
+     <p>Version: v1</p>';
+  res.end(body);
+}).listen(port, function(){
+  console.log('Server running at port ' + port);
+});
